40996bd3d6994f3e8ce7a9e68f8eac90558702e7
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-16 00:00:00 +0000

    Correction stricts types.

--- src/Service/Form/GenerateurFormFactory.php
+++ src/Service/Form/GenerateurFormFactory.php
@@ -9,7 +9,7 @@ class GenerateurFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $form = new GenerateurForm(null, $options);
+        $form = new GenerateurForm(null, $options ?? []);
         $form->setApi($services->get('ViewHelperManager')->get('api'));
         return $form;
     }
